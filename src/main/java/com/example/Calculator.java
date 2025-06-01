23c0922b424914b4e7d59af244c2e5b9bcf0a76b
Roshan <v.c.roshan@gmail.com>
2025-06-01 20:05:55 +0530

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -6,8 +6,7 @@ package com.example;
 public class Calculator {
 
     public int add(int a, int b) {
-        system.out.print(a+b)
-        return a + b;
+         return a + b;
     }
 
     public int subtract(int a, int b) {
